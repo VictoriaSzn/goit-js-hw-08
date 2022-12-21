0a5efbf28dcfd76b098dcde4b08c99792f87f488
VictoriaSzn <victoriia.szn@gmail.com>
2022-12-21 18:48:15 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,11 +4,17 @@ import throttle from 'lodash.throttle';
 const vplayer = document.querySelector('#vimeo-player');
 
 const player = new Player(vplayer);
-
+checkCurrentTime();
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay ({seconds}){
     localStorage.setItem('videoplayer-current-time', seconds);
 };
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+function checkCurrentTime(){
+    if (!localStorage.getItem('videoplayer-current-time')) {
+        return;
+}
+ 
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
